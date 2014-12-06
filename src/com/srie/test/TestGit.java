38860351963340d8bb3815d89532ea4d5ab4b463
stono888 <stono_wang@163.com>
2014-12-06 21:43:38 +0800

    commit 9

--- src/com/srie/test/TestGit.java
+++ src/com/srie/test/TestGit.java
@@ -3,7 +3,7 @@ package com.srie.test;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("asdf");
+		System.out.println("asdf9999999");
 		System.out.println("commit 1");
 	}
 
